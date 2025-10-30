f122d219b4f97805913bf577cd2735585e8c6da7
Aarav Shukla <aarav.shukla105@gmail.com>
2025-10-31 01:51:49 +0530

    Added the font styling library

---
+++ lib/font.ts
@@ -0,0 +1,13 @@
+import { Sofia_Sans_Condensed, Spline_Sans_Mono } from "next/font/google";
+
+export const sofiasanscondensed = Sofia_Sans_Condensed({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    display: "swap"
+})
+
+export const splinesansmono = Spline_Sans_Mono({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    display: "swap"
+})
